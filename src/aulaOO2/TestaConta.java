1e44a6691132b378128009985fc5f4839d5ec032
36118482018.2@senacrjedu.local <36118482018.2@senacrjedu.local>
2019-04-13 11:05:40 -0300

    teste alterando o valor da variavel

--- src/aulaOO2/TestaConta.java
+++ src/aulaOO2/TestaConta.java
@@ -7,7 +7,7 @@ public class TestaConta {
 	public static void main(String[] args) {
 		Conta c1 = new Conta();
 	
-		c1.numero = 001;
+		c1.numero = 003;
 		c1.nome = "Joao";
 		c1.saldo = 100.0;
 		c1.limite = 10.0;
